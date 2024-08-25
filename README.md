6313728daaa6747aa9d8fb060aad58f87cc630d5
Nishanth <97822858+NishanthBalaji@users.noreply.github.com>
2024-08-25 21:49:48 +0530

    Added img expand feature

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # expanding-image-cards
+
+This project is an interactive image gallery developed using HTML, CSS, and JavaScript. Users can click on any image to expand it into a larger view, which overlays the screen for a closer look. The design is responsive, ensuring a smooth and engaging user experience across all devices.
---
+++ app.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel')
+let totalImg = panels.length;
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
